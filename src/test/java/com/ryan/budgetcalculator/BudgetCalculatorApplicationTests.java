22c9735c739ff03029fb6ce785f63c44b7681862
ryandev1004 <ryansullivan1004@gmail.com>
2025-01-25 19:27:58 -0500

    Initial commit

---
+++ src/test/java/com/ryan/budgetcalculator/BudgetCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ryan.budgetcalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetCalculatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
