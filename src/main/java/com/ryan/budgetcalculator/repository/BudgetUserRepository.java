330827206cd73abe94cded0f79ab9e27eb61a79f
ryandev1004 <ryansullivan1004@gmail.com>
2025-01-27 23:13:28 -0500

    Added parameters to JpaRepository

--- src/main/java/com/ryan/budgetcalculator/repository/BudgetUserRepository.java
+++ src/main/java/com/ryan/budgetcalculator/repository/BudgetUserRepository.java
@@ -1,8 +1,11 @@
 package com.ryan.budgetcalculator.repository;
 
+import com.ryan.budgetcalculator.entity.BudgetUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.UUID;
+
 @Repository
-public interface BudgetUserRepository extends JpaRepository {
+public interface BudgetUserRepository extends JpaRepository<BudgetUser, UUID> {
 }
