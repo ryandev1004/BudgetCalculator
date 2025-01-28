0994f79a1a360858c221b6c8e63537363ca4a0da
ryandev1004 <ryansullivan1004@gmail.com>
2025-01-27 23:12:41 -0500

    Added methods for getting all budget users, creating budget users, and deleting budget users.

--- src/main/java/com/ryan/budgetcalculator/controller/BudgetUserController.java
+++ src/main/java/com/ryan/budgetcalculator/controller/BudgetUserController.java
@@ -1,13 +1,13 @@
 package com.ryan.budgetcalculator.controller;
 
+import com.ryan.budgetcalculator.entity.dto.BudgetUserCreateDTO;
 import com.ryan.budgetcalculator.entity.dto.BudgetUserDTO;
+import com.ryan.budgetcalculator.examples.repository.PondMemberRepository;
 import com.ryan.budgetcalculator.repository.BudgetUserRepository;
 import com.ryan.budgetcalculator.service.BudgetUserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.UUID;
@@ -18,9 +18,26 @@ public class BudgetUserController {
 
     private final BudgetUserService budgetUserService;
 
+
     @GetMapping("users")
     public ResponseEntity<List<BudgetUserDTO>> getBudgetAllUsers() {
         return ResponseEntity.ok().body(budgetUserService.getAllBudgetUsers());
+    }
+
+    @GetMapping("users/{id}")
+    public ResponseEntity<BudgetUserDTO> getBudgetUserById(@PathVariable UUID id) {
+        return null;
+    }
+
+    @PostMapping("users")
+    public ResponseEntity<BudgetUserDTO> addBudgetUser(@RequestBody BudgetUserCreateDTO budgetUserCreateDTO) {
+        return ResponseEntity.ok().body(budgetUserService.createBudgetUser(budgetUserCreateDTO));
+    }
+
+    @DeleteMapping("users/{id}")
+    public ResponseEntity<UUID> deleteBudgetUser(@PathVariable UUID id) {
+        budgetUserService.deleteBudgetUser(id);
+        return ResponseEntity.ok().body(id);
     }
 
 
