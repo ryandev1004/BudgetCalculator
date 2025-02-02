a14ef72f2897ac76365aad8749f7d0859c3a6958
Nick Doxa <77412044+NickDoxa@users.noreply.github.com>
2025-02-02 16:31:01 -0500

    created budget report entity and added foreign key to budget user

---
+++ src/main/java/com/ryan/budgetcalculator/entity/BudgetReport.java
@@ -0,0 +1,33 @@
+package com.ryan.budgetcalculator.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.hibernate.annotations.UuidGenerator;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Data
+@RequiredArgsConstructor
+public class BudgetReport {
+
+    @Id
+    @UuidGenerator
+    private UUID id;
+
+    @Column(columnDefinition = "TEXT")
+    private String reportDetails;
+
+    private LocalDate reportDate;
+
+    private double postMonthlyIncome;
+
+    private double postYearlyIncome;
+
+    @ManyToOne
+    @JoinColumn(name = "budget_user_id")
+    private BudgetUser budgetUser;
+
+}
--- src/main/java/com/ryan/budgetcalculator/entity/BudgetUser.java
+++ src/main/java/com/ryan/budgetcalculator/entity/BudgetUser.java
@@ -1,11 +1,10 @@
 package com.ryan.budgetcalculator.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Data;
 import org.hibernate.annotations.UuidGenerator;
 
+import java.util.Set;
 import java.util.UUID;
 
 @Entity
@@ -16,14 +15,26 @@ public class BudgetUser {
     @Id
     @UuidGenerator
     private UUID id;
+
     private String name;
+
     private int age;
+
     private String occupation;
+
     private double yearlyIncome;
+
     private double monthlyRent;
+
     private double monthlyFoodCost;
+
     private double monthlyStreamingAndWifi;
+
     private double monthlyInsurance;
+
     private double monthlyOtherExpenses;
+
+    @OneToMany(mappedBy = "budgetUser", cascade = CascadeType.ALL)
+    private Set<BudgetReport> budgetReports;
 
 }
