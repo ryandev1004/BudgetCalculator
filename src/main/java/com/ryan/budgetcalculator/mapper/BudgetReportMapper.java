ddebe8a4a76993f2f07b5f79501656188ff88295
ryandev <ryansullivan1004@gmail.com>
2025-05-24 16:05:06 -0400

    Created Post Endpoint for Budget Reporting

--- src/main/java/com/ryan/budgetcalculator/mapper/BudgetReportMapper.java
+++ src/main/java/com/ryan/budgetcalculator/mapper/BudgetReportMapper.java
@@ -1,12 +1,16 @@
 package com.ryan.budgetcalculator.mapper;
 
 import com.ryan.budgetcalculator.entity.BudgetReport;
+import com.ryan.budgetcalculator.entity.dto.BudgetReportCreateDTO;
 import com.ryan.budgetcalculator.entity.dto.BudgetReportDTO;
-import org.mapstruct.Mapper;
+import org.mapstruct.*;
 
 @Mapper(componentModel = "spring")
 public interface BudgetReportMapper {
 
-    BudgetReportDTO toDto(BudgetReport entity);
+    BudgetReportDTO toDTO(BudgetReport budgetReport);
+
+    @Mapping(target = "reportId", ignore = true)
+    BudgetReport fromCreateDTO(BudgetReportCreateDTO dto);
 
 }
